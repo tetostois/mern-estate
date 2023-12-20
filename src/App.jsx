eac502af6af2913e4415de6b625219688eafb198
KAMGA <tetakamgasteve@gmail.com>
2023-12-20 05:07:47 -0500

    Creation des pages et des Routes(Chemins)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignUp from "./pages/SignUp";
+import About from './pages/About';
+import SignIn from "./pages/SignIn";
+
 export default function App() {
-  return <h1 className="text-red-500">App</h1>
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element= {<Home />} />
+      <Route path="/sign-in" element= {<SignIn />} />
+      <Route path="/sign-up" element= {<SignUp />} />
+      <Route path="/profil" element= {<Profile />} />
+      <Route path="/about" element= {<About />} />
+    </Routes>
+  </BrowserRouter>
 }
